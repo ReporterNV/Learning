590dbe89ad1f509abe3bae603a171235bbc56cb2
Your Name <you@example.com>
2024-03-24 02:05:34 +0700

    start code

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace CSharp
+{
+	internal class Program{
+		static void Main(string[] args){
+			Console.WriteLine("Hello, World!");
+			char [,] map = null;
+			string[]file = File.ReadAllLines("map.txt");
+			Console.WriteLine(file[0]);
+
+		}
+	}
+}
