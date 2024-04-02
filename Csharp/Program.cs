c91865429ed30881b66864e321962f5b37cdf015
Your Name <you@example.com>
2024-04-03 02:08:42 +0700

    add basic methods

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -4,10 +4,40 @@ namespace CSharp
 	internal class Program{
 		static void Main(string[] args){
 			Console.WriteLine("Hello, World!");
-			char [,] map = null;
-			string[]file = File.ReadAllLines("map.txt");
-			Console.WriteLine(file[0]);
+			char [,] map = ReadMap("map.txt");
+			DrawMap(map);
+			
+		}
 
+		private static void DrawMap(char[,] map)
+		{
+			for(int y = 0; y < map.GetLength(1); y++){
+				for(int x = 0; x < map.GetLength(0); x++)
+					Console.Write(map[x,y]);
+				Console.WriteLine();
+			}
+
+
+		}
+
+		private static char[,] ReadMap(string path)
+		{
+			string[]file = File.ReadAllLines("map.txt");
+			char [,] map = new char[GetMaxLengthOfLine(file), file.Length];
+			Console.WriteLine("INIT MAP {0:D} {0:D}", file[0].Length, file.Length);
+			for(int y = 0; y < GetMaxLengthOfLine(file); y++)
+				for(int x = 0; x < file.Length; x++)
+					map[x, y] = file[y][x];
+			return map;
+		}
+
+		private static int GetMaxLengthOfLine(string[] lines)
+		{
+			int MaxLength = lines.Length;
+			foreach(var line in lines)
+				if(line.Length > MaxLength)
+					MaxLength = line.Length;
+			return MaxLength;
 		}
 	}
 }
